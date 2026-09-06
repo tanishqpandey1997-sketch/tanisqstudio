a6353680ec489e061099cf93f731efd90b71b2bb
Tanisq pandey <tanisqpandey07@Tanisqs-MacBook-Air.local>
2026-09-07 04:13:02 +0530

    Replace e-commerce mockup image with ecommerce.png

--- framerusercontent.com/sites/4OrEgOLGdSfOIOkKewCsF8/https/framerusercontent.com/modules/3wVzbEXAl1Xy42tdLJFe/rhOcGE0cwmIRkozLNOsi/augiA20Il.js
+++ framerusercontent.com/sites/4OrEgOLGdSfOIOkKewCsF8/https/framerusercontent.com/modules/3wVzbEXAl1Xy42tdLJFe/rhOcGE0cwmIRkozLNOsi/augiA20Il.js
@@ -2015,10 +2015,10 @@ const Component = /*#__PURE__*/ React.forwardRef(function(props, ref) {
                                                 width: "100%"
                                             },
                                             UHHeeF5QI: addImageAlt({
-                                                pixelHeight: 4500,
-                                                pixelWidth: 6e3,
-                                                src: "/aiagencymock.png",
-                                                srcSet: "https://framerusercontent.com/images/UbFctpOpIXsg78nbVuz7cQKOj4.webp?scale-down-to=512&width=6000&height=4500 512w,https://framerusercontent.com/images/UbFctpOpIXsg78nbVuz7cQKOj4.webp?scale-down-to=1024&width=6000&height=4500 1024w,https://framerusercontent.com/images/UbFctpOpIXsg78nbVuz7cQKOj4.webp?scale-down-to=2048&width=6000&height=4500 2048w,https://framerusercontent.com/images/UbFctpOpIXsg78nbVuz7cQKOj4.webp?scale-down-to=4096&width=6000&height=4500 4096w,https://framerusercontent.com/images/UbFctpOpIXsg78nbVuz7cQKOj4.webp?width=6000&height=4500 6000w"
+                                                pixelHeight: 1024,
+                                                pixelWidth: 1536,
+                                                src: "/ecommerce.png",
+                                                srcSet: "/ecommerce.png 1536w"
                                             }, ""),
                                             variant: matchVariant("h3UYmb6_l"),
                                             width: "100%",
